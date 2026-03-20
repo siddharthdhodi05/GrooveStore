6c71e90152cd4445e43f029031a3ac328620d2ef
siddharthdhodi05 <siddharth.dhodi.work@gmail.com>
2026-03-20 20:21:34 +0530

    Step 27 completed:      - Added add to cart redux action

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -7,7 +7,39 @@ const initialState = localStorage.getItem("cart")
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.cartItems.find(
+        (cartItem) => cartItem._id === item._id,
+      );
+      if (existingItem) {
+        state.cartItems = state.cartItems.map((cartItem) => {
+          return cartItem._id === existingItem._id ? item : cartItem;
+        });
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+      // Calculate items price
+      state.itemsPrice = state.cartItems.reduce(
+        (acc, currVal) => acc + currVal.price * currVal.qty,
+      );
+
+      // Calculate shipping price
+      state.shippingPrice = state.itemPrice > 5000 ? 0 : 2000;
+
+      // calculate tax price
+      state.taxPrice = 0.18 * state.itemsPrice;
+
+      // calculate total price
+      state.totalPrice =
+        state.itemsPrice + state.shippingPrice + state.taxPrice;
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
